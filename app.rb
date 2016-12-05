04dc524795c277b3ff2a3b8d673cf81b7ae71e33
Joshua Rinard and Luke Philips <joshua@>
2016-12-05 11:24:43 -0800

    Inital files plus workthrough of lesson

---
+++ app.rb
@@ -0,0 +1,19 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "to_do_josh"})
+
+get("/") do
+  @tasks = Task.all()
+  erb(:index)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  task = Task.new(description)
+  task.save()
+  erb(:success)
+end
